21dc9ae34986f855878e44fd1317122c72e24bf2
Ricardo <rferromoreno@gmail.com>
2017-05-20 17:41:24 -0300

    Modifiqué seeder y gitignore

--- database/seeds/TazasTableSeeder.php
+++ database/seeds/TazasTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use Illuminate\Http\File;
 
 class TazasTableSeeder extends Seeder
 {
@@ -18,31 +19,31 @@ class TazasTableSeeder extends Seeder
         DB::table('tazas')->insert([
             [
                 'nombre' => $faker->streetName,
-                'taza' => 'public/images/taza1.png',
+                'taza' => Storage::putFile('public/images', new File('/images/taza1.png')),
                 'created_at' => Carbon::now()
             ],
             [
                 'nombre' => $faker->streetName,
-                'taza' => 'public/images/taza2.png',
+                'taza' => Storage::putFile('public/images', new File('/images/taza2.png')),
                 'created_at' => Carbon::now()
             ],
             [
                 'nombre' => $faker->streetName,
-                'taza' => 'public/images/taza3.png',
+                'taza' => Storage::putFile('public/images', new File('/images/taza3.png')),
                 'created_at' => Carbon::now()
             ],
             [
                 'nombre' => $faker->streetName,
-                'taza' => 'public/images/taza4.png',
+                'taza' => Storage::putFile('public/images', new File('/images/taza4.png')),
                 'created_at' => Carbon::now()
             ],
             [
                 'nombre' => $faker->streetName,
-                'taza' => 'public/images/taza5.png',
+                'taza' => Storage::putFile('public/images', new File('/images/taza5.png')),
                 'created_at' => Carbon::now()
             ],[
                 'nombre' => $faker->streetName,
-                'taza' => 'public/images/taza6.png',
+                'taza' => Storage::putFile('public/images', new File('/images/taza6.png')),
                 'created_at' => Carbon::now()
             ]
         ]);
