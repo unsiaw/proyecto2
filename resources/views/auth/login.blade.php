0abc626ec02c97ca83fddb33ef7792c3fc6b7b32
matiaschap <matias.marzu@gmail.com>
2017-05-20 18:13:51 -0300

    visual de registrar y logear

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,74 +2,95 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Login</div>
+            <div class="panel-body">
+                <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <label for="email" class="col-md-4 control-label"></label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+                        <div class="col-md-6">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                            <input id="email" type="email" class="form-control" placeholder="E-Mail" name="email" value="{{ old('email') }}" required autofocus>
+                        </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <label for="password" class="col-md-4 control-label"></label>
+
+                        <div class="col-md-6">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                            <input id="password" type="password" class="form-control" placeholder="Password" name="password" required>
+                        </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                </label>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                    <div class="form-group">
+                        <div class="col-md-8 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary">
+                                Logear
+                            </button>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                Olvidó su Password?
+                            </a>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                            <p>  <h3>                              
+                       <!--          <a href="redirect/facebook">Facebook Login</a>
+                                <a href="redirect/twitter">Twitter Login</a>
+                                <a href="redirect/github">Github Login</a>
+                                <a href="redirect/google">Google Login</a> -->
+
+
+    <div class="col-sm-6 social-buttons">
+      <a class="btn btn-block btn-social btn-facebook" href="redirect/facebook">
+        <span class="fa fa-facebook"></span> Sign in with Facebook
+      </a>
+      <a class="btn btn-block btn-social btn-twitter" href="redirect/twitter">
+        <span class="fa fa-twitter"></span> Sign in with Twitter
+      </a>
+      <a class="btn btn-block btn-social btn-github" href="redirect/github">
+        <span class="fa fa-github"></span> Sign in with GitHub
+      </a>
+      <a class="btn btn-block btn-social btn-google" href="redirect/google">
+        <span class="fa fa-google"></span> Sign in with Google
+      </a>
+    </div>
+
+                            </p>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-
-                                <p>                                
-                                    <a href="redirect/facebook">Facebook Login</a>
-                                    <a href="redirect/twitter">Twitter Login</a>
-                                    <a href="redirect/github">Github Login</a>
-                                    <a href="redirect/google">Google Login</a>
-                                </p>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
--- resources/views/layouts/estilos.blade.php
+++ resources/views/layouts/estilos.blade.php
@@ -4,6 +4,9 @@
     <link href="{{ URL::to('/css/bootstrap-theme.min.css') }}" id="theme" rel="stylesheet">
     <!-- Estilos propios -->
     <link href="{{ URL::to('/css/custom.css') }}" rel="stylesheet">
+    <!-- Estilos Social-Connect -->
+    <link href="{{ URL::to('/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ URL::to('/css/bootstrap-social.css') }}" rel="stylesheet" >
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
