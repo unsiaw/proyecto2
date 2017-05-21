19a86728d553054ffd88650b525b5391f853d9b1
matiaschap <matias.marzu@gmail.com>
2017-05-21 03:59:58 -0300

    vista autos #45 #46

--- resources/views/autos/show.blade.php
+++ resources/views/autos/show.blade.php
@@ -3,28 +3,47 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-8 well">
+            <div class="col-xs-12 col-sm-6 col-md-8 col-md-8 well">
                 <div class="hidden-xs"><h2>Personalizá tu auto</h2></div>
                 <div id="miContenedor" class="col-xs-12 full-width well">
                     <canvas id="imagenAuto"></canvas>
                 </div>
             </div>
+        
+    
+    
+        <div class="col-xs-12 col-sm-6 col-md-8 col-md-4">
+            <h4>Link público para compartir: </h4><div class="alert alert-info well well-sm" id="p1"><a href="{{route('autos.share',$auto->token)}}">{{route('autos.share',$auto->token)}}</a></div>
+            <button class="btn btn-primary" onclick="copyToClipboard('#p1')">Copiar Link</button>
+                
+                <div class="col-md-8"><h3><br>Datos del Chasis</h3>
+            <ul class="list-group">
+                    <li class="list-group-item"><h4>ID: {{ $auto->id }}</h4></li>
+                    <li class="list-group-item"><h4>Chasis ID: {{ $auto->chasis_id }}</h4></li>
+                    <li class="list-group-item"><h4>Taza ID: {{ $auto->taza_id }}</h4></li>
+                    <li class="list-group-item"><h4>Color: {{ $auto->color }}</h4></li>
+            </ul>
+        </div>
+
+
         </div>
     </div>
-    <div class="container">
-        <div class="col-xs-12 col-sm-6 col-md-8">
-            Enlace para compartir: <div class="well well-sm">{{route('autos.share',$auto->token)}}</div>
-        </div>
     </div>
 
-    {{ $auto->id }}
-    {{ $auto->chasis_id }}
-    {{ $auto->taza_id }}
-    {{ $auto->color }}
+
 @endsection
 
 
 @section('scripts')
+    <script>
+        function copyToClipboard(elemento) {
+             var $temp = $("<input>")
+                $("body").append($temp);
+                $temp.val($(elemento).text()).select();
+                document.execCommand("copy");
+                $temp.remove();
+        }
+    </script>
     <script>
         $( document ).ready(function() {
             canvas = new fabric.Canvas('imagenAuto');
--- resources/views/tazas/taza.blade.php
+++ resources/views/tazas/taza.blade.php
@@ -4,7 +4,6 @@
     <td>{{ $taza->nombre }}</td>
     <td>{{ $taza->created_at->diffForHumans() }}</td>
     <td>
-    	<a class="btn btn-primary" href="">Edit</a>
     	<a class="btn btn-info" href="{{ route('tazas.uno', $taza->id) }}">Ver</a>
     	<a class="btn btn-danger" href="">Delete</a></td>
    </td>
