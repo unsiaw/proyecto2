4dbe3c0b43f3c2a941bc5cb15542e88223b5d4ba
matiaschap <matias.marzu@gmail.com>
2017-05-21 00:28:22 -0300

    vista chasis,tazas y autos

--- resources/views/autos/show.blade.php
+++ resources/views/autos/show.blade.php
@@ -14,8 +14,13 @@
             });
         });
     </script>
-    {{ $auto->id }}
-    {{ $auto->chasis_id }}
-    {{ $auto->taza_id }}
-    {{ $auto->color }}
+        <div class="col-md-4"></div>
+        <div class="col-md-3"><h3>Datos del Auto</h3>
+            <ul class="list-group">
+                    <li class="list-group-item"><h4>ID: #{{ $auto->id }}</h4></li>
+                    <li class="list-group-item"><h4>Chasis ID: {{ $auto->chasis_id}}</h4></li>
+                    <li class="list-group-item"><h4>Taza ID: {{ $auto->taza_id }}</h4></li>
+                    <li class="list-group-item"><h4>Color {{ $auto->color}}</h4></li>
+            </ul>
+        </div>
 @endsection
--- resources/views/tazas/show.blade.php
+++ resources/views/tazas/show.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $tazas->id }}
-    {{ $tazas->nombre }}
-    <img src="{{ Storage::url($tazas->taza) }}" />
+        <div class="col-md-3"></div>
+        <div class="col-md-1"><br><br><br><br><img src="{{ Storage::url($tazas->taza) }}" /></div>
+        <div class="col-md-3">
+        	<ul class="list-group"><h3>Datos de la Taza</h3>
+  					<li class="list-group-item"><h4>Numero: #{{ $tazas->id }}</h4></li>
+  					<li class="list-group-item"><h4>Nombre: {{ $tazas->nombre }}</h4></li>
+			</ul>
+		</div>
 @endsection
