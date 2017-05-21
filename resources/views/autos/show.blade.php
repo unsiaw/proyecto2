e8def17b03ddb91d9f36cd0656fb4edee30b3951
Ricardo <rferromoreno@gmail.com>
2017-05-21 15:03:04 -0300

    Arregle otro typo en la vista del auto

--- resources/views/autos/show.blade.php
+++ resources/views/autos/show.blade.php
@@ -20,7 +20,7 @@
         <div class="col-md-12">
             <button class="btn btn-primary" onclick="copyToClipboard('#p1')">Copiar Link</button>
          </div>       
-                <div class="col-md-8"><h3><br>Datos del Chasis</h3>
+                <div class="col-md-8"><h3><br>Datos del auto</h3>
             <ul class="list-group">
                     <li class="list-group-item"><h4>ID: {{ $auto->id }}</h4></li>
                     <li class="list-group-item"><h4>Chasis ID: {{ $auto->chasis_id }}</h4></li>
