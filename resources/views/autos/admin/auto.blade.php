8b65fa480417b9a6cecccba1f4b3452790392a8b
matiaschap <matias.marzu@gmail.com>
2017-05-20 22:04:32 -0300

    tablas con botones

--- resources/views/autos/admin/auto.blade.php
+++ resources/views/autos/admin/auto.blade.php
@@ -3,6 +3,5 @@
     <th scope="row">{{ $auto->id }}</th>
     <td>{{ $auto->created_at->diffForHumans() }}</td>
     <td>{{ $auto->user->name }}</td>
-    <td><a href="{{ route('autos.uno', $auto->id) }}">Ver</a></td>
+    <td><a class="btn btn-info" href="{{ route('autos.uno', $auto->id) }}">Ver</a></td>
 </tr>
-
--- resources/views/autos/auto.blade.php
+++ resources/views/autos/auto.blade.php
@@ -2,6 +2,6 @@
 <tr>
     <th scope="row">{{ $auto->id }}</th>
     <td>{{ $auto->created_at->diffForHumans() }}</td>
-    <td><a href="{{ route('autos.uno', $auto->id) }}">Ver</a></td>
+    <td><a class="btn btn-info" href="{{ route('autos.uno', $auto->id) }}">Ver</a></td>
 </tr>
 
